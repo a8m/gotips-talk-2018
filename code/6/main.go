c4645d85e8896deb120626c8c9729a281d3f4b4a
Ariel Mashraki <ariel@mashraki.co.il>
2018-01-05 13:23:15 +0200

    fix 6

--- code/6/main.go
+++ code/6/main.go
@@ -11,4 +11,8 @@ func main() {
 }
 
 // Notes:
-// 1. Doesn't work well with vendor. see: #15478.
+// 1. Useful when you're working with people that don't have
+//    Go environemt installed, and you still want to split
+//    your project into packages. For example, submitting your
+//    University HW.
+// 2. Doesn't work well with vendor. see: #15478.
