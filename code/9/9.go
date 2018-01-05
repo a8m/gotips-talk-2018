4c9d426a254eca9298ceaa7bfff0584e0aab3d81
Ariel Mashraki <ariel@mashraki.co.il>
2018-01-05 13:34:13 +0200

    Update 9.go

--- code/9/9.go
+++ code/9/9.go
@@ -1,11 +1,5 @@
 package main
 
-import "fmt"
-
-func main() {
-	fmt.Println(Parse("(()))"))
-}
-
 func Parse(s string) (err error) {
 	defer func() {
 		if e := recover(); e != nil {
@@ -16,25 +10,25 @@ func Parse(s string) (err error) {
 			err = pe
 		}
 	}()
+	// parse may panic. if the input is invalid.
 	parse(s)
 	return
 }
 
-// Notes:
+// Notes from "Go Wiki - PanicAndRecover"
 //
-// Useful pattern when "every step" is expectation.
-// Some examples in the std:
+// By convention, no explicit panic() should be allowed to cross a package boundary.
+// Indicating error conditions to callers should be done by returning error value.
+// Within a package, however, especially if there are deeply nested calls to non-exported
+// functions, it can be useful (and improve readability) to use panic to indicate error
+// conditions which should be translated into error for the calling function.
+// Below is an admittedly contrived example of a way in which a nested function and an
+// exported function may interact via this panic-on-error relationship.
+// 
+// Some examples from the std:
 // - json/decode.go
 // - go/parser.go
 // - text/template/parse.go
 // - encoding/gob/decode.go "catchError"
 //
-// For more info, see: https://github.com/golang/go/wiki/PanicAndRecover.
-//
-// > By convention, no explicit panic() should be allowed to cross a package boundary.
-// > Indicating error conditions to callers should be done by returning error value.
-// > Within a package, however, especially if there are deeply nested calls to non-exported
-// > functions, it can be useful (and improve readability) to use panic to indicate error
-// > conditions which should be translated into error for the calling function.
-// > Below is an admittedly contrived example of a way in which a nested function and an
-// > exported function may interact via this panic-on-error relationship.
+// Read more here: https://github.com/golang/go/wiki/PanicAndRecover.
